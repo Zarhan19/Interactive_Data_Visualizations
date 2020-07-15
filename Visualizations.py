a6bd978c26fb7e8c89d039db9a93845ebd4afdfc
Nahda Azeez <NahdAzeez@gmail.com>
2020-07-15 15:09:02 +0300

    Function to visualize Bar graph with drop down menu

--- Visualizations.py
+++ Visualizations.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from IPython.display import display,Image # Allows the use of display() for DataFrames
+from IPython.display import display,Image,HTML # Allows the use of display() for DataFrames
 import HelperFunctions as hf
 import Visualizations as viz
 
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
+from ipywidgets import widgets
 
     
 def multi_line(df):
@@ -54,3 +54,63 @@ def scatter_plot(df,x_val,y_val,frame_,group_,title_):
                range_y=[df[y_val].min(),df[y_val].max()],
                title=title_)
     return plot_
+
+def bar_dropdown(df):
+    #df = merge_df
+    df_initial = df[(df['Commodity']=='COTTON.') & (df['country'] == "IRAQ")]
+    df2 = df_initial[['year','Export_Value','Import_Value']].groupby(['year']).sum().reset_index()
+
+
+    drop_down_1 = widgets.Dropdown(
+        description='Commodity:   ',
+        value = 'COTTON.',
+        options=df['Commodity'].unique().tolist()    )
+
+    drop_down_2 = widgets.Dropdown(
+        options=list(df['country'].unique()),
+        value = 'IRAQ',
+        description='Country:')
+
+
+    # Assign an empty figure widget with two traces
+    trace1 = go.Bar(x=df2['year'],y = df2['Export_Value'], name='Export Value')
+    #trace1
+    trace2 = go.Bar(x=df2['year'], y = df2['Import_Value'], name='Import Value')
+
+    g = go.FigureWidget(data=[trace1,trace2],
+                        layout=go.Layout(
+                            title=dict(
+                                text='Export vs Import '
+                            ),
+                            barmode='group'
+                        ))
+
+    def response(change):
+        filter_list = [i and j for i, j in
+                               zip(df['Commodity'] == drop_down_1.value
+                                   , df['country'] == drop_down_2.value
+                                  )]
+        temp_df = df[filter_list]
+
+        y1 = temp_df['Export_Value']
+        #print(x1)
+        y2 = temp_df['Import_Value']
+        x1 = temp_df['year']
+        with g.batch_update():
+            g.data[0].x = x1
+            g.data[0].y = y1
+            g.data[1].x = x1
+            g.data[1].y = y2
+            #g.layout.barmode = 'overlay'
+            g.layout.xaxis.title = 'Year'
+            g.layout.yaxis.title = 'value'
+
+            #g.layout.yaxis = temp_df['year']
+
+
+    drop_down_1.observe(response, names="value")
+    drop_down_2.observe(response, names="value")
+
+    container = widgets.HBox([drop_down_1, drop_down_2])
+#     widgets.VBox([container,g])
+    return g,container
